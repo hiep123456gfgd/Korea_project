ff12f6550831658c86f195eea9369333cc64aff5
hiệp trần <tranhieptk2003@gmail.com>
2024-11-30 09:12:51 +0700

    Mô tả thay đổi của bạn

--- routes/api.php
+++ routes/api.php
@@ -22,31 +22,31 @@ Route::post('/dat-mk',[CustomerController::class,'datmk']);
 Route::post('/login', [CustomerController::class, 'login']);
 Route::post('/register', [CustomerController::class, 'register']);
 
-Route::get('/admin/customer/data', [CustomerController::class, 'getData']);
-Route::post('/admin/customer/create', [CustomerController::class, 'createData']);
-Route::put('/admin/customer/update', [CustomerController::class, 'UpateData']);
-Route::delete('/admin/customer/delete/{id}', [CustomerController::class, 'deleteData']);
+Route::get('/customer/data', [CustomerController::class, 'getData']);
+Route::post('/customer/create', [CustomerController::class, 'createData']);
+Route::put('/customer/update', [CustomerController::class, 'UpateData']);
+Route::delete('/customer/delete/{id}', [CustomerController::class, 'deleteData']);
 
-Route::get('/admin/comments/data', [CommentsController::class, 'getData']);
-Route::post('/admin/comments/create', [CommentsController::class, 'createData']);
-Route::put('/admin/comments/update', [CommentsController::class, 'UpateData']);
-Route::delete('/admin/comments/delete/{id}', [CommentsController::class, 'deleteData']);
+Route::get('/comments/data', [CommentsController::class, 'getData']);
+Route::post('/comments/create', [CommentsController::class, 'createData']);
+Route::put('/comments/update', [CommentsController::class, 'UpateData']);
+Route::delete('/comments/delete/{id}', [CommentsController::class, 'deleteData']);
 
-Route::get('/admin/posts/data', [PostsController::class, 'getData']);
-Route::post('/admin/posts/create', [PostsController::class, 'createData']);
-Route::put('/admin/posts/update', [PostsController::class, 'UpateData']);
-Route::delete('/admin/posts/delete/{id}', [PostsController::class, 'deleteData']);
+Route::get('/posts/data', [PostsController::class, 'getData']);
+Route::post('/posts/create', [PostsController::class, 'createData']);
+Route::put('/posts/update', [PostsController::class, 'UpateData']);
+Route::delete('/posts/delete/{id}', [PostsController::class, 'deleteData']);
 
-Route::get('/admin/pages/data', [PagesController::class, 'getData']);
-Route::post('/admin/pages/create', [PagesController::class, 'createData']);
-Route::put('/admin/pages/update', [PagesController::class, 'UpateData']);
-Route::delete('/admin/pages/delete/{id}', [PagesController::class, 'deleteData']);
+Route::get('/pages/data', [PagesController::class, 'getData']);
+Route::post('/pages/create', [PagesController::class, 'createData']);
+Route::put('/pages/update', [PagesController::class, 'UpateData']);
+Route::delete('/pages/delete/{id}', [PagesController::class, 'deleteData']);
 
-Route::post('/admin/likes/create', [LikesController::class, 'createData']);
-Route::put('/admin/likes/update', [LikesController::class, 'UpateData']);
-Route::get('/admin/likes/posts/data', [LikesController::class, 'getData']);
-Route::get('/admin/likes/pages/data', [LikesController::class, 'getData']);
+Route::post('/likes/create', [LikesController::class, 'createData']);
+Route::put('/likes/update', [LikesController::class, 'UpateData']);
+Route::get('/likes/posts/data', [LikesController::class, 'getData']);
+Route::get('/likes/pages/data', [LikesController::class, 'getData']);
 
-Route::post('/admin/friendships/create', [LikesController::class, 'createData']);
+Route::post('/friendships/create', [LikesController::class, 'createData']);
 
 
